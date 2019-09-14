3b31ba9b4126ae55de816baca41cfbdd6b03ad71
Abdul Aziz Tetteh <abdulaziztetteh@gmail.com>
2019-09-14 21:08:13 +0000

    register models to admin site and update settings with media folder structure

--- btre/btre/settings.py
+++ btre/btre/settings.py
@@ -122,3 +122,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
+
+# Media Folder Settings
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
