672ca66316f94e37627d7a23760594862482e6d1
Abdul Aziz Tetteh <abdulaziztetteh@gmail.com>
2019-09-15 08:19:51 +0000

    preserve form input

--- btre/listings/views.py
+++ btre/listings/views.py
@@ -61,5 +61,6 @@ def search(request):
         "bedroom_choices": bedroom_choices,
         "price_choices": price_choices,
         "listings": queryset_list,
+        "values": request.GET,
     }
     return render(request, "listings/search.html", context)
